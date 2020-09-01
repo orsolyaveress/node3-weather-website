3eebdc43694609ed692e9574980e4f679967128d
Veress Orsolya <veress_orsolya@4dsoft.hu>
2020-09-01 10:46:00 +0200

    added humidity to forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('postman-request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=c7a01aa2979fb6074eda833329ddbcf7&query=' + longitude + ',' + latitude
   request({url, json: true}, (error, { body } ) => {
@@ -8,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location!', undefined)
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
+    
     }
   })
 }
